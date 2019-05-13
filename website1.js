40d7ef853152827150bd923c3e78bac6a736d9cb
OlimjonNematov <40324936+OlimjonNematov@users.noreply.github.com>
2019-05-13 13:52:47 -0500

    Add files via upload

---
+++ website1.js
@@ -0,0 +1,10 @@
+$function(){
+  var docEl=$(document),
+    animEl=$('bodySection');
+  docEl.on('scroll',funtion(){
+    var curScrollPos=docEl.scrollTop();
+    animEl.each(function(){
+      var $this=this
+    });
+  });
+}
